eed8355fb7ff5f88ba7f6abac52bacf06acd7b41
tomus1857 <tomus1857.2001@o2.pl>
2024-10-20 12:35:56 +0200

    Initial commit

---
+++ ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package net.javaguides.ems.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping("/student")
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
---
+++ ems-backend/src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -0,0 +1,18 @@
+package net.javaguides.ems.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmployeeDto {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
